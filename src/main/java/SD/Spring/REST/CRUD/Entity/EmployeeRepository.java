80e62eb2b156d537dadd1596a57c56ba65a5bc94
sdjalili <Git@286J2379>
2023-10-19 00:33:19 +0330

    Spring data REST/JPA additional interface class to reduce DAO/Service/REST classes.

---
+++ src/main/java/SD/Spring/REST/CRUD/Entity/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package SD.Spring.REST.CRUD.Entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
