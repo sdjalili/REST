ab3f9c17f2dd04df2440a6cd9a9dc0c906d1752b
sdjalili <147697307+sdjalili@users.noreply.github.com>
2023-10-19 11:00:56 +0330

    Delete src/main/java/SD/Spring/REST/CRUD/DAO directory

--- src/main/java/SD/Spring/REST/CRUD/DAO/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package SD.Spring.REST.CRUD.DAO;
-
-import SD.Spring.REST.CRUD.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path="members")
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
