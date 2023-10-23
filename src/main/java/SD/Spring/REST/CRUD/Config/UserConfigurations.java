640d1f73dba13352f370b031fd6a87e64a75b1b3
jalilisd@gmail.com <Git@286J2379>
2023-10-23 18:21:54 +0330

    OK

---
+++ src/main/java/SD/Spring/REST/CRUD/Config/UserConfigurations.java
@@ -0,0 +1,12 @@
+package SD.Spring.REST.CRUD.Config;
+
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserConfigurations {
+
+
+
+}
